6ae391264be8717c86f0acb2fee313e489f3b7cc
Eric Adamski <er.adamski@gmail.com>
2016-10-06 14:33:59 -0400

    initial commit

---
+++ spec/snippet-manager-view.spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SnippetManagerView from '../lib/snippet-manager-view';
+
+describe('SnippetManagerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
